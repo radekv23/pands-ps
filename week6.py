b5f7e889f6f54e37a7bef6c725ddeee2324bdd4a
R W <r.wojtczak23@gmail.com>
2020-02-25 21:04:51 +0000

    week6-2

--- week6.py
+++ week6.py
@@ -10,7 +10,7 @@ def sqrt (s):
     return sq
 
 #getting input
-s=float(input("Enter second value to be squared: "))
+s=float(input("Enter value to be squared: "))
 
 #printing result
 print(sqrt(s))
