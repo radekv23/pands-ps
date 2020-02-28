e78acbb3fa9b4609ad9afbe2355e9823f044a151
R W <r.wojtczak23@gmail.com>
2020-02-28 11:12:37 +0000

    adding file variable

--- week7.py
+++ week7.py
@@ -1,9 +1,10 @@
 #Write a program that reads in a text file and outputs the number of e's it contains. 
 # The program should take the filename from an argument on the command line.
 
+file=input("Enter your file name:")
 counter = 0
  
-with open("houn.txt", 'r') as f:
+with open(file, 'r') as f:
     for line in f:
         words = line.split()
         for i in words:
