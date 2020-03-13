3633ac974bc4ac088890d0a9d1a438144fd72bd1
R W <r.wojtczak23@gmail.com>
2020-03-13 13:43:04 +0000

    Feedback correction

--- Collatz.py
+++ Collatz.py
@@ -4,22 +4,22 @@
 #  but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
 
 #asking for input
-positive = int(input("Enter posivive value:"))
+a = int(input("Enter posivive value:"))
 
 #if input is negative value print message and stop
-while positive < 0:
-    print (positive,"is not a positive number")
-    break
+if a < 0:
+    print (a,"is not a positive number")
+   # break
 
 #prints input when positive
-while positive > 0:
-    print (positive)
-    break
+elif a > 0:
+    print (a)
+  #  break
 
 #perform calculations until equals 1
-while positive > 1:
-    if positive % 2 == 0:
-        positive =  int(positive/2)
+if a > 1:
+    if a % 2 == 0:
+        a =  int(a/2)
     else:
-        positive = int((positive*3)+1)
-    print (positive)
+        a = int((a*3)+1)
+    print (a)
