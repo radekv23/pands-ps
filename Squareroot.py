e65024172297b98f6f7710006259ebec827c94fe
R W <r.wojtczak23@gmail.com>
2020-03-15 13:11:52 +0000

    squareroot recomendation fix

--- Squareroot.py
+++ Squareroot.py
@@ -2,16 +2,23 @@
 # Radoslaw Wojtczak - G00352936
 #  creating a function called sqrt that does this.
 
-#importing math functions
-import math
-
 #creating function called sqrt
-def sqrt (s):
-    sq=math.sqrt(s)
-    return sq
-
+def sqrt (s)
+    sq=s ** 0.5
 #getting input
 s=float(input("Enter your value to be squared: "))
 
 #printing result
 print(sqrt(s))
+
+
+#Second version delet '#'
+
+#import math
+#def sqrt (s):
+  #  sq=math.sqrt(s)
+  #  return sq
+
+  #s=float(input("Enter your value to be squared: "))
+
+#print(sqrt(s))
