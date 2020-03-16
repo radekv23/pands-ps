40f342169045bcbc344f5b74b9d81fd027d4ea56
R W <r.wojtczak23@gmail.com>
2020-03-16 15:58:20 +0000

    improve plot

--- functionPlot.py
+++ functionPlot.py
@@ -2,22 +2,26 @@
 # a program that displays a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 
 # in the range [0, 4] on the one set of axes.
 
-#importing matplotlib and numpy
+#importing matplotlib and numpy 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# setting up variables
-x = np.linspace(1.0, 4.0, 100)
-g = x**2
-h = x**3
+# Setting range from 0 to 4
+x= np.arange(0,4)
 
-#ploting
-plt.plot(x, g,'g.', label="squared")
-plt.plot(x, h,'r.', label="**3")
+# calculating y values
+y = x
+y2 = 2 * x
+y3 = x*x
+
+# Adding graph title and labels to X and Y axis
+plt.title("Function Plot x (green), 2x (red), x2 (blue); range = [0, 4]")
+plt.xlabel("x axis") 
+plt.ylabel("y axis") 
+
+# plot may or may not plot the lines, depending on the arguments
+plt.plot(x,y,'g.')
+plt.plot(x,y2,'r.')
+plt.plot(x,y3,'b.')
+
 plt.show()
-
-#plot legend
-plt.legend()
-plt.title("First Plot")
-plt.xlabel("x")
-plt.ylabel("y")
